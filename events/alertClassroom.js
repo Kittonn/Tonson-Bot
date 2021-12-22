d518f3db2e7be1239c2553865252507acdc71a49
Kitton._ <tonkittipod@gmail.com>
2021-12-23 00:51:43 +0700

    update Embed

--- events/alertClassroom.js
+++ events/alertClassroom.js
@@ -1,18 +1,27 @@
 const cron = require('node-cron')
 const {MessageEmbed} = require('discord.js')
+const {channelId} = require('../config.json')
 
 module.exports = {
     'name': 'ready',
     execute(client) {
-        const channel = client.channels.cache.get('923224129077903481')
-        cron.schedule('30 23 * * wed', async () => {
+
+        const channel = client.channels.cache.get(channelId)
+
+        //================================| Monday |=======================================
+
+        cron.schedule('47 0 * * thu', async () => {
 
             const Embed = new MessageEmbed()
                             .setColor('#57F2AA')
-                            .setTitle('Alert Classroom')
-                            .setDescription('https://us02web.zoom.us/j/6365107396?pwd=RFI0UmcrQ0xib1FRNzNIdXNyK2FmUT09'
-                                            + "\n Meeting ID: 636 510 7396"
-                                            + '\n Passcode: 994407')
+                            .setTitle(':alarm_clock: Alert Classroom')
+                            .setDescription(':timer: **Time** : .........'
+                                            +'\n\n:books: **Subject** : บลาๆๆๆๆ'
+                                            +'\n\n:teacher: **Teacher** : ................'
+                                            +'\n\n:link: **Link** : https://us02web.zoom.us/j/6365107396?pwd=RFI0UmcrQ0xib1FRNzNIdXNyK2FmUT09'
+                                            + "\n\n:bookmark: **Classroom** : .........."
+                                            )
+                                    
 
             await channel.send({ embeds: [Embed] })
         },{
