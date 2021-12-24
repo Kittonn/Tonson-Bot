badcb7df864429a04d8c98f9b72dab5542e29126
Kitton._ <tonkittipod@gmail.com>
2021-12-25 00:54:58 +0700

    update country stats covid

--- commands/interactions/covid.js
+++ commands/interactions/covid.js
@@ -1,4 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js')
+const axios = require('axios')
+
+const url_covid_country = 'https://coronavirus-19-api.herokuapp.com/countries'
+
+const date = new Date().toLocaleString()
+
+const arrCovid = async () => {
+    const res = await axios.get(url_covid_country)
+    return res
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,9 +22,38 @@ module.exports = {
     async execute(interaction) {
 
         const args = interaction.options.getString('country')
-        await interaction.reply(`${args}`)
 
-        
+        const value = await arrCovid()
+        const data = value.data
+
+        for (let i = 0; i < data.length; i++) {
+            if (args === data[i].country) {
+
+                const country = data[i].country
+                const cases = data[i].cases
+                const todayCases = data[i].todayCases
+                const deaths = data[i].deaths
+                const todayDeaths = data[i].todayDeaths
+                const recovered = data[i].recovered
+                
+                const covidEmbed = new MessageEmbed()
+                    .setColor('#001524')
+                    .setTitle(`:microbe: รายงานยอดผู้ติดเชื้อประเทศ ${country}`)
+                    .setDescription(`\`อัพเดตข้อมูล ${date}\``)
+                    .addFields(
+                        {name: 'ติดเชื้อเพิ่มวันนี้', value: `${todayCases}`, inline: true},
+                        {name: 'ติดเชื้อสะสมในประเทศ', value: `${cases}`, inline: true},
+                        {name: 'เสียชีวิตเพิ่ม', value: `${todayDeaths}`, inline: true},
+                        {name: 'เสียชีวิตรวม', value: `${deaths}`, inline: true},
+                        {name: 'รักษาหายรวม', value: `${recovered}`, inline: true}
+                    )
+                    
+                    
+                await interaction.reply({embeds: [covidEmbed]})
+
+            }
+            
+        }
 
     }
 }
