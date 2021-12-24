0e648cc4ce499b306d008987b3976baf9488210f
Kitton._ <tonkittipod@gmail.com>
2021-12-24 12:03:57 +0700

    update dropdown menu command

--- events/selectCommand.js
+++ events/selectCommand.js
@@ -4,10 +4,17 @@ const commands = require('../config/interactionHandler')
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction) {
+        
         if (!interaction.isSelectMenu()) return;
 
+        const command = commands.get(interaction.values[0])
+
+        if (!command) return
+
+        
+
         if (interaction.customId === 'select') {
-            await interaction.reply('Hello test');
+            await command.execute(interaction)
         }
     }
 }
