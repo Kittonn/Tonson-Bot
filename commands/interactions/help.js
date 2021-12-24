5b73ca6f6934f81388e68c5ccc99895793ad2d8c
Kitton._ <tonkittipod@gmail.com>
2021-12-24 10:06:21 +0700

    update help command and new covid slash command

--- commands/interactions/help.js
+++ commands/interactions/help.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 
 const help = new MessageEmbed()
     .setColor('#03045e')
@@ -8,10 +9,54 @@ const help = new MessageEmbed()
                     '\n\n**:school: Class Schedule** \n`;schedule` = `ดูตารางสอน online`'+ 
                     '\n\n**:memo: TCAS65 Schudule**\n `;gatpat` = `ดูตารางสอบ Gat & Pat และดูเวลาเตรียมตัว`'+ 
                     '\n `;saman` = `ดูตารางสอบ 9 วิชาสามัญ และดูเวลาเตรียมตัว` \n `;tcas65` = `ดูปฏิทิน TCAS65`'+ 
-                    '\n\n **:speech_left: Message API** \n `;covid19 thailand` = `ดูรายงานโควิดประจำวัน`\n`;covid ชื่อจังหวัด` = `ดูรายงานโควิดประจำจังหวัด`'+
+                    '\n\n **:speech_left: Message API** \n`;covid` = `ดูรายงานโควิดประจำประเทศไทย`'+
                     ' \n `;inspire` = `ดูแรงบันดาลใจ`'+
                     '\n`;watasalim` = `ดูวาทกรรมสลิ่ม`'+
                     '\n\n **:mag_right: About** \n `;dev` = `ข้อมูลผู้พัฒนา`')
+
+const row = new MessageActionRow()
+.addComponents(
+    new MessageSelectMenu()
+        .setCustomId('select')
+        .setPlaceholder('Nothing selected')
+        .addOptions([
+            {
+                label: 'Schedule',
+                description: 'Send Class Schedule.',
+                value: 'schedule',
+            },
+            {
+                label: 'Gatpat',
+                description: 'Send Gat & Pat Schedule.',
+                value: 'gatpat',
+            },
+            {
+                label: 'Saman',
+                description: 'Send Saman Schedule.',
+                value: 'saman',
+            },
+            {
+                label: 'Tcas65',
+                description: 'Send Tcas65 Schedule.',
+                value: 'tcas65',
+            },
+            {
+                label: 'Inspirational Quote',
+                description: 'Send Inspirational Quote.',
+                value: 'inspire',
+            },
+            {
+                label: 'Watasalim Quote',
+                description: 'Send Watasalim Quote.',
+                value: 'watasalim',
+            },
+            {
+                label: 'Developer Information',
+                description: 'Send Developer Information.',
+                value: 'dev',
+            }
+        ]),
+);
 
 
 module.exports = {
@@ -19,6 +64,6 @@ module.exports = {
         .setName('help')
         .setDescription('Send All Commands.'),
     async execute(interaction) {
-        return interaction.reply({ embeds: [help] });
+        return interaction.reply({ embeds: [help], components: [row] });
     },
 };
